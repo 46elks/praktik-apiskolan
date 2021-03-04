4a16e2399df468b2656dbcbdbc3d9cf1b846ee8d
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-03-04 11:37:57 +0100

    added simple test for logo in navbar

--- tests/webtests/test_header.py
+++ tests/webtests/test_header.py
@@ -15,3 +15,12 @@ class TestHeader(WebTestBase):
         # Assert if specific text is in navbar
         self.assertIn("Om oss", navbar_names)
         self.assertIn("Kontakt", navbar_names)
+
+    def test_logo_link(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        navbar = driver.find_element(By.ID, "mainNav")
+
+        # Attempts to find navbar logo
+        navbar.find_element(By.CLASS_NAME, "navbar-brand")
