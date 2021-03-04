db9f2f4ddd54feef4cd47f2bf27c1e0824204a02
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-03-04 10:07:48 +0100

    simple test to check if footer exists

---
+++ tests/webtests/test_footer.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from web_test_base import WebTestBase
+import requests
+
+class TestFooter(WebTestBase):
+
+    def test_exists(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        # Attempts to find footer element on page
+        driver.find_element(By.TAG_NAME, "footer")
