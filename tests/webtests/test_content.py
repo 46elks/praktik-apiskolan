818e2ab2bfa3e3896fedc7fc440eb44dde5fa5f4
klado <klaudiusz.pawezka@elev.ntig.se>
2021-03-17 15:51:16 +0100

    Added two new test cases for quiz part

--- tests/webtests/test_content.py
+++ tests/webtests/test_content.py
@@ -24,7 +24,7 @@ class TestContentPage(WebTestBase):
 
         self.assertTrue(result)
 
-def test_quiz(self):
+    def test_quiz_correct(self):
         driver = self.driver
         driver.get(self.WEBSITE_URL)
         
@@ -33,5 +33,57 @@ def test_quiz(self):
         content_button = navbar_nav.find_element(By.XPATH, "//a[@href='content.html']")
         content_button.click()
 
+        # Looks for the right answer
         quiz = driver.find_element(By.ID, "quiz")
+        correct_answer = quiz.get_attribute("data-answer")
+        
+        # Sets right answer
+        answer_try = quiz.find_element(By.TAG_NAME, "input")
+        answer_try_click = answer_try.find_element(By.XPATH, "//input[@value=" + correct_answer + "]")
+        answer_try_click.click()
+
+        # Clicks check button
+        button = quiz.find_element(By.TAG_NAME, "input")
+        button_click = button.find_element(By.XPATH, "//input[@value='Svara']")
+        button_click.click()
+
+        # Reads alert text and controls it
+        alert = driver.switch_to.alert
+        alert_text = alert.text
+        self.assertIn("rätt", alert_text.lower())
+        alert.accept()
+
+    def test_quiz_wrong(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+        
+        # Attempts to navigate to content page
+        navbar_nav = driver.find_element(By.CLASS_NAME, "navbar-nav")
+        content_button = navbar_nav.find_element(By.XPATH, "//a[@href='content.html']")
+        content_button.click()
+
+        # Amount of answers
+        quiz_answers = driver.find_element(By.ID, "quizAnswers")
+        amount_quiz_answers = len(quiz_answers.find_elements(By.XPATH, "//input[@name='quizOption']"))
+
+        # Looks for the right answer
+        quiz = driver.find_element(By.ID, "quiz")
+        correct_answer = quiz.get_attribute("data-answer")
+        wrongAnswer = (int(correct_answer) + 1) % amount_quiz_answers
+
+        # Sets right answer
+        answer_try = quiz.find_element(By.TAG_NAME, "input")
+        answer_try_click = answer_try.find_element(By.XPATH, "//input[@value=" + str(wrongAnswer) + "]")
+        answer_try_click.click()
+
+        # Clicks check button
+        button = quiz.find_element(By.TAG_NAME, "input")
+        button_click = button.find_element(By.XPATH, "//input[@value='Svara']")
+        button_click.click()
+
+        # Reads alert text and controls it
+        alert = driver.switch_to.alert
+        alert_text = alert.text
+        self.assertIn("fel", alert_text.lower())
+        alert.accept()
 
