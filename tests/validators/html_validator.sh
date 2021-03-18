3765091889c9aafe2eb4064540a96602d1b5e1c3
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-03-18 14:21:28 +0100

    fixed validation CI
    Also added error handling so the issue won't go unnoticed again

--- tests/validators/html_validator.sh
+++ tests/validators/html_validator.sh
@@ -6,13 +6,18 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
+let "file_amount=0"
+
 # Goes through all files ending in .html.
 for file in $(find $1 -iname '*.html'); do
+    let "file_amount++"
+
     # Sends a HTML validation request to the validator.w3.org API. Return the data in JSON format.
     data=`curl -s -H "Content-Type: text/html" \
     --data-binary @$file \
     https://validator.w3.org/nu/?out=json \
     \;`
+
     if [[ $data == *"type"* ]]; then
         # If the result data contains the text "type", an error has occured.
         # In this case, print the error and return 1 as exit status.
@@ -22,5 +27,10 @@ for file in $(find $1 -iname '*.html'); do
     fi
 done
 # No errors in validation, return 0 as exit status.
-echo "No errors in HTML-document(s)"
-exit 0
+if [ $file_amount != 0 ]; then
+    echo "No errors in HTML file(s)"
+    exit 0
+else
+    echo "No HTML files found"
+    exit 1
+fi
