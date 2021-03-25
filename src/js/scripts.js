1eadbc7cce241a1ccd6db00e658bf7b135bb339a
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-03-25 11:26:55 +0100

    call to action now hidden on content page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,15 @@
+// Hide all elements with specific ID outside of home page.
+$(function(){
+    console.log(window.location.pathname)
+    if (window.location.pathname != "/dist/index.html" && window.location.pathname != "/dist/") {
+          $('#hideOutsideHomePage').hide();
+    } else {
+          $('#hideOutsideHomePage').show();
+    }
+});
+
+
+// Quiz System
 function showQuizResult(quizName) {
     let quiz = document.getElementsByName(quizName)[0];
     let quizValue = quiz.quizOption.value;
