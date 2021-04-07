324b9318d605912185a52c9ed713d34a5cacf02e
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-04-07 11:58:00 +0200

    Real links in footer (#20)
    * updated footer with real links
    
    * copyright text now links to 46elks.se
    
    * updated favicon tests

--- tests/webtests/test_favicon.py
+++ tests/webtests/test_favicon.py
@@ -8,4 +8,12 @@ class TestFavicon(WebTestBase):
         driver = self.driver
         driver.get(self.WEBSITE_URL)
 
+        driver.find_element(By.XPATH, "//link[@rel='apple-touch-icon']")
         driver.find_element(By.XPATH, "//link[@rel='icon']")
+        driver.find_element(By.XPATH, "//link[@rel='manifest']")
+        driver.find_element(By.XPATH, "//link[@rel='mask-icon']")
+        driver.find_element(By.XPATH, "//link[@rel='shortcut icon']")
+
+        driver.find_element(By.XPATH, "//meta[@name='msapplication-TileColor']")
+        driver.find_element(By.XPATH, "//meta[@name='msapplication-config']")
+        driver.find_element(By.XPATH, "//meta[@name='theme-color']")
