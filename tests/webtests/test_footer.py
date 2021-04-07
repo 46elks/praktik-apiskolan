a794275e7a3db73f55d071441e9e7273bcbc09eb
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-04-07 10:36:52 +0200

    updated footer with real links

--- tests/webtests/test_footer.py
+++ tests/webtests/test_footer.py
@@ -4,9 +4,25 @@ from web_test_base import WebTestBase
 
 class TestFooter(WebTestBase):
 
-    def test_exists(self):
+    def test_links(self):
         driver = self.driver
         driver.get(self.WEBSITE_URL)
 
-        # Attempts to find footer element on page
-        driver.find_element(By.TAG_NAME, "footer")
+        # Gets footer element on page
+        footer = driver.find_element(By.TAG_NAME, "footer")
+
+        # Gets all links in footer
+        link_elements = footer.find_elements(By.TAG_NAME, "a")
+
+        # Adds link names to array
+        links = []
+        for element in link_elements:
+            links.append(element.get_attribute("href"))
+
+        # Asserts if links are present in footer
+        self.assertIn("https://github.com/46elks", links)
+        self.assertIn("https://medium.com/46elks", links)
+        self.assertIn("https://twitter.com/46elks", links)
+        self.assertIn("https://www.instagram.com/46elks/", links)
+        self.assertIn("https://www.facebook.com/46elks", links)
+
