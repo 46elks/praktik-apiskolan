28839cfca3831c8c86107a4f25255d4d1ba98eb2
klado <klaudiusz.pawezka@elev.ntig.se>
2021-04-15 15:59:23 +0200

    Code editor can write output on website console

--- dist/js/editor.js
+++ dist/js/editor.js
@@ -1,12 +1,37 @@
 // Retrieve Elements
+const consoleLogList = document.querySelector(".editor-console-logs");
 const executeCodeButton = document.querySelector(".editor-run");
 const resetCodeButton = document.querySelector(".editor-reset");
 
 // Setup Ace
 let codeEditor = ace.edit("editorCode");
 let defaultCode = 'console.log("Hello World!")';
+let consoleMessages = [];
 
 let editorLib = {
+    clearConsoleScreen() {
+        consoleMessages.length = 0;
+
+        // Remove all elements in the log list
+        while (consoleLogList.firstChild) {
+            consoleLogList.removeChild(consoleLogList.firstChild);
+        }
+
+    },
+    printToConsole() {
+        consoleMessages.forEach(log => {
+            const newLogItem = document.createElement('li');
+            const newLogText = document.createElement('pre');
+
+            newLogText.className = log.class;
+            newLogText.textContent = `>  ${log.message}`;
+
+            newLogItem.appendChild(newLogText);
+
+            consoleLogList.appendChild(newLogText);
+        })
+
+    },
     init() {
         // Configure Ace
 
@@ -29,6 +54,9 @@ let editorLib = {
 
 // Events
 executeCodeButton.addEventListener('click', () => {
+    // Clear console massages
+    editorLib.clearConsoleScreen();
+
     // Get input from the code editor
     const userCode = codeEditor.getValue();
 
@@ -38,11 +66,17 @@ executeCodeButton.addEventListener('click', () => {
     } catch (err) {
         console.error(err);
     }
+
+    // Print to the console
+    editorLib.printToConsole();
 });
 
 resetCodeButton.addEventListener('click', () => {
     // Clear ace editor
     codeEditor.setValue(defaultCode);
+
+    // Clear console massages
+    editorLib.clearConsoleScreen();
 });
 
 editorLib.init();
