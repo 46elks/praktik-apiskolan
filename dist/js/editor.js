d59a5c42fc0e2d92f6f152b9803f2b83f674af97
klado <klaudiusz.pawezka@elev.ntig.se>
2021-04-15 14:48:04 +0200

    Configuration of ace editor
    Buttons for run and reset are working now, added dracula theme and javascript mode with auto completion

--- dist/js/editor.js
+++ dist/js/editor.js
@@ -1,1 +1,48 @@
+// Retrieve Elements
+const executeCodeButton = document.querySelector(".editor-run");
+const resetCodeButton = document.querySelector(".editor-reset");
+
+// Setup Ace
 let codeEditor = ace.edit("editorCode");
+let defaultCode = 'console.log("Hello World!")';
+
+let editorLib = {
+    init() {
+        // Configure Ace
+
+        // Theme
+        codeEditor.setTheme("ace/theme/dracula");
+
+        // Set language
+        codeEditor.session.setMode("ace/mode/javascript");
+
+        // Set Options
+        codeEditor.setOptions({
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+        });
+
+        // Set default Code
+        codeEditor.setValue(defaultCode)
+    }
+}
+
+// Events
+executeCodeButton.addEventListener('click', () => {
+    // Get input from the code editor
+    const userCode = codeEditor.getValue();
+
+    // Run the user code
+    try {
+        new Function(userCode)();
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+resetCodeButton.addEventListener('click', () => {
+    // Clear ace editor
+    codeEditor.setValue(defaultCode);
+});
+
+editorLib.init();
