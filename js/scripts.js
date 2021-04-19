c94aa36a0c0624867921a9d4b9cddbbe4310d8a1
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-04-19 09:58:09 +0200

    moved js folder to dist folder (#45)

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function showQuizResult(quizName) {
+    let quiz = document.getElementsByName(quizName)[0];
+    let quizValue = quiz.quizOption.value;
+    let quizAnswer = quiz.getAttribute("data-answer")
+    let quizAnswerText = quiz.querySelector('[value="' + quizAnswer + '"]').parentElement.textContent.trim();
+
+    if (quizValue==quizAnswer)
+        alert("Rätt svar, bra jobbat!");
+    else
+        alert('Nästan! Rätt svar är "' + quizAnswerText + '".');
+}
